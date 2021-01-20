c37678383fc7e7d518272b78618985e8e7abcd81
leslie-huang <lesliehuang@nyu.edu>
2021-01-20 15:07:10 -0500

    update

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="pairwise_distance",
+    name="pairwisedistance",
     version="0.0.1",
     author="Leslie Huang",
     author_email="lesliehuang@nyu.edu",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/leslie-huang/distinctiveness_with_embeddings",
+    url="https://github.com/leslie-huang/pairwise_distance",
     packages=find_packages(),
     classifiers=["Programming Language :: Python :: 3"],
 )
